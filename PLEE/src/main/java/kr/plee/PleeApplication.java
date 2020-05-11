682e674d4c05f17bf09d6698be55ffb6382c4b53
pleeeeee <65173580+pleeeeee@users.noreply.github.com>
2020-05-11 22:41:55 +0900

    Add files via upload

---
+++ PLEE/src/main/java/kr/plee/PleeApplication.java
@@ -0,0 +1,19 @@
+package kr.plee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class PleeApplication {
+
+	@RequestMapping("/")
+    String home() {        
+        return "Hello World!";
+    }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PleeApplication.class, args);
+	}
+
+}
---
+++ PLEE/src/test/java/kr/plee/PleeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.plee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PleeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
